cb06094a99b593ef8fb464bbf2d0963ae93404ff
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-20 19:45:48 +0200

    Create Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,66 +2,55 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Dashboard</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-buttons slot="end">
+            <ion-button color="danger" @click.prevent="logout">
+              Log out
+            </ion-button>
+          </ion-buttons>
+          <ion-title>Dashboard</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <card header="Dashboard" color="primary" variant="danger">
+          <ion-text>
+            Welcome, <i>{{ $store.state.auth.user.name }}</i>!
+          </ion-text>
+        </card>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import Card from "../components/Card.vue";
+import { loadingController } from "@ionic/vue";
 
 export default {
-  name: 'Home',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
+  name: "Home",
+
+  components: { Card },
+
+  methods: {
+    async logout() {
+      const loading = await loadingController.create({
+        message: "Please wait...",
+      });
+
+      loading.present();
+
+      this.$store
+        .dispatch("auth/logout")
+        .then(() => loading.dismiss())
+        .catch(() => loading.dismiss());
+    },
+  },
 };
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
