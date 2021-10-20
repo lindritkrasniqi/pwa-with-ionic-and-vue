ddfcea5a1d16b8e7298aec0f91c12d49e43a9517
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-20 10:31:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pwa-with-ionic-and-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
