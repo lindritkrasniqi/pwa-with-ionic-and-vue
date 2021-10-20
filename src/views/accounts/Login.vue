5eef49f28c3a77be7149ef4af3781ab152dd340b
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-20 19:47:50 +0200

    Create ionic pages login, reset, forgot and signup

---
+++ src/views/accounts/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div id="container">
+        <card header="Login" color="primary" variant="danger">
+          <forms-login />
+        </card>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import Card from "../../components/Card.vue";
+import FormsLogin from "../../components/forms/Login.vue";
+
+export default {
+  name: "Login",
+
+  components: { Card, FormsLogin },
+};
+</script>
+
+<style scoped>
+#container {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
---
+++ src/views/accounts/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div id="container">
+        <card header="Sign up" color="primary" variant="danger">
+          <forms-signup />
+        </card>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import Card from "../../components/Card.vue";
+import FormsSignup from "../../components/forms/Signup.vue";
+
+export default {
+  name: "signup",
+
+  components: { Card, FormsSignup },
+};
+</script>
+
+<style scoped>
+#container {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
